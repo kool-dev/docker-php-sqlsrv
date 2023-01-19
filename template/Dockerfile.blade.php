3022135059bc1176ae2c208e6fa63dea1dd0bcd9
Leonardo Souza <souzaleo97@hotmail.com>
2023-01-19 15:21:45 -0300

    update ODBC driver to version 17.10

--- template/Dockerfile.blade.php
+++ template/Dockerfile.blade.php
@@ -4,11 +4,11 @@ FROM {{ $from }}
 
 RUN set -xe \
     # Download the desired package(s)
-    && curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.7.1.1-1_amd64.apk \
-    && curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.7.1.1-1_amd64.apk \
+    && curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.10.2.1-1_amd64.apk \
+    && curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.10.1.1-1_amd64.apk \
     #Install the package(s)
-    && apk add --allow-untrusted msodbcsql17_17.7.1.1-1_amd64.apk \
-    && apk add --allow-untrusted mssql-tools_17.7.1.1-1_amd64.apk \
+    && apk add --allow-untrusted msodbcsql17_17.10.2.1-1_amd64.apk \
+    && apk add --allow-untrusted mssql-tools_17.10.1.1-1_amd64.apk \
     && apk add --no-cache --virtual .persistent-deps freetds unixodbc \
     && apk add --no-cache --virtual .build-deps $PHPIZE_DEPS unixodbc-dev freetds-dev \
     && docker-php-source extract \
